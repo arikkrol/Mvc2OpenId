e02acfa9e0fed4e71e851814f5cc0f7d21dd778a
Arik Krol <arik.krol@picscout.com>
2012-04-23 16:10:43 +0300

    addition of AuthenticationMAnager

---
+++ OpenIdRelyingPartyMvc/Models/AuthenticationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetOpenAuth.OpenId.RelyingParty;
+
+namespace OpenIdRelyingPartyMvc.Models
+{
+    public static class AuthenticationManager
+    {
+        private static OpenIdRelyingParty s_Openid = new OpenIdRelyingParty();
+
+        public static IAuthenticationResponse GetAuthenticationResponse()
+        {
+            return s_Openid.GetResponse();
+        }
+
+        public static IAuthenticationRequest MakeAuthenticationRequest(string openIdProviderUrl)
+        {
+            return s_Openid.CreateRequest(openIdProviderUrl);
+        }
+    }
+}
