73bdd1b21055fe1e658c15f8de7955aa5996cdfd
Arik Krol <arik.krol@picscout.com>
2012-04-23 09:49:19 +0300

    First commit - before uploading to GitHub

---
+++ OpenIdProviderMvc/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿namespace OpenIdProviderMvc.Controllers {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Web;
+	using System.Web.Mvc;
+
+	[HandleError]
+	public class HomeController : Controller {
+		public ActionResult Index() {
+			if (Request.AcceptTypes.Contains("application/xrds+xml")) {
+				ViewData["OPIdentifier"] = true;
+				return View("Xrds");
+			}
+
+			ViewData["Message"] = "Welcome to ASP.NET MVC!";
+			return View();
+		}
+
+		public ActionResult About() {
+			return View();
+		}
+
+        /// <summary>
+        /// This is the entry point to the OpenId provider service!!!
+        /// </summary>
+        /// <returns></returns>
+		public ActionResult Xrds() {
+			ViewData["OPIdentifier"] = true;
+			return View();
+		}
+	}
+}
